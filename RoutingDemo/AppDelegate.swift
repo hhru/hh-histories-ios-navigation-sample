6ce170ebf225f8ae64a9b32b208ed5a1e9f670e3
Timur Shafigullin <hometim55@gmail.com>
2021-12-20 19:47:01 +0300

    Push notification handling

--- RoutingDemo/AppDelegate.swift
+++ RoutingDemo/AppDelegate.swift
@@ -13,11 +13,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         appearance.tintColor = Colors.important
     }
 
+    private func setupUserNotificationCenter() {
+        let center = UNUserNotificationCenter.current()
+
+        center.delegate = self
+
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            print("requestAuthorization() -> granted: \(granted)")
+            print("requestAuthorization() -> error: \(error as Any)")
+        }
+    }
+
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         setupNavigationBarAppearance()
+        setupUserNotificationCenter()
 
         return true
     }
@@ -31,5 +43,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             name: "Main",
             sessionRole: connectingSceneSession.role
         )
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        defer {
+            completionHandler()
+        }
+
+        let userInfo = response.notification.request.content.userInfo
+
+        guard let roomID = userInfo["room_id"] as? Int, let chatID = userInfo["chat_id"] else {
+            return
+        }
+
+        print("didReceive(roomID: \(roomID), chatID: \(chatID))")
+
+        // Perform navigation to private room
+        // with authorization check and open chat from list
     }
 }
